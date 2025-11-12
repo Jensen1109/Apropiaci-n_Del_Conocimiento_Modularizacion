acae7fc8f5bebea5344ddbefb90713b7a5f8a31a
Jensen1109 <rodriguezjensen11@gmail.com>
2025-11-11 21:47:00 -0500

    se realizo el 2 ejercicio donde se calculo el promedio de 3 notas y retorno el promedio del estudiante ademas se verifica de que el promedio sea mayor o igual a 3.0 para aprobar de resto reprobo

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-function SaludoPersonalizado(nombre)
-{
-    return "Bienvendio" + nombre;
+// Se define una función llamada SaludoPersonalizado
+// Esta función recibe un parámetro llamado nombre
+function SaludoPersonalizado(nombre) {
     
+    // La función devuelve un mensaje que dice "Bienvenido" seguido del nombre recibido
+    return "Bienvenido " + nombre;
 }
+
+// Se llama a la función SaludoPersonalizado con el argumento 
+// El resultado se muestra en la consola
 console.log(SaludoPersonalizado("Jensen"));
+
+// Se llama a la función SaludoPersonalizado con el argumento 
+// El resultado se muestra en la consola
 console.log(SaludoPersonalizado("Cristiano"));
+
 
